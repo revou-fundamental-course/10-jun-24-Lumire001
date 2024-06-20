02ae4c2026d6d99de6e2c4610f49ee8ea27f76c1
Lumire001 <fathoni.penting@gmail.com>
2024-06-20 22:10:26 +0700

    finish

--- js/script.js
+++ js/script.js
@@ -1,34 +1,50 @@
+
+var display_c = document.getElementById("nama_c");
+var display_f = document.getElementById("nama_f");
+var celcius_d = document.getElementById("celcius");
+var farenheit_d = document.getElementById("farenheit");
+var current_stage = "Celcius (°C)"
 function konversi(){
-    let angka_celcius = document.getElementById("celcius").value;
-    if (angka_celcius){
-        let celcius = angka_celcius;
-        if(!isNaN(celcius)){
-            let konversi = (parseFloat(celcius * (9/5)) + 32);
-            document.getElementById("farenheit").value = konversi;
-            console.log(konversi);
-        }else{
-            console.log("Mohon masukan angka");
-            document.getElementById("farenheit").value = "Mohon masukan angka";
-    } 
+    var celcius = parseFloat(document.getElementById("celcius").value);
+    var formula = document.getElementById("rumus").innerText;
+    /*Kalkulasi Celcius*/
+    var farenheit_k = (celcius * 9/5) + 32
+    formula_ganti1 = formula.replace("S(°C)", celcius)
+    /*Kalkulasi Farenheit*/
+    var farenheit_c = (celcius - 32) * 5/9
+    var formula_d = "S(°C) = (a - 32) x 5/9"
+    formula_ganti2 = formula_d.replace("a", celcius)
+    /*Eksekusi*/
+    if (display_c.textContent === "Celcius (°C)"){
+        var hasil_1 = document.getElementById("farenheit").value = farenheit_k + "°F"
+        document.getElementById("kalkulasi").textContent = formula_ganti1 + " = " + hasil_1
+    }if(display_c.textContent === "Farenheit (°F)"){
+        var hasil_2 = document.getElementById("farenheit").value = farenheit_c + "°C"
+        document.getElementById("kalkulasi").textContent = formula_ganti2 + " = " + hasil_2
     }
+    console.log(celcius)
 }
 
 function reset(){
-    document.getElementById("celcius").value = "";
-    document.getElementById("farenheit").value = "";
+    document.getElementById("celcius").value = ""
+    document.getElementById("farenheit").value = ""
+    document.getElementById("kalkulasi").innerText = ""
 }
 
 function revert(){
-    let angka_farenheit = document.getElementById("farenheit").value;
-    if (angka_farenheit){
-        let farenheit = angka_farenheit;
-        if(!isNaN(farenheit)){
-            let revert = (parseFloat(farenheit - 32) / 9/5);
-            document.getElementById("celcius").value = revert;
-            console.log(revert);
-        }else{
-            console.log("Mohon masukan angka");
-            document.getElementById("celcius").value = "Mohon masukan angka";
-    } 
+    if(display_c.textContent === "Farenheit (°F)"){
+        display_c.textContent = "Celcius (°C)"
+        display_f.textContent = "Farenheit (°F)"
+        celcius_d.setAttribute("placeholder", "Masukan Suhu Celcius (°C)")
+        farenheit_d.setAttribute("placeholder", "Hasil Suhu Farenheit (°F)")
+
+    } else{
+        display_c.textContent = "Farenheit (°F)"
+        display_f.textContent = "Celcius (°C)"
+        celcius_d.setAttribute("placeholder", "Masukan Suhu Farenheit (°F)")
+        farenheit_d.setAttribute("placeholder", "Hasil Suhu Celcius (°C)")
     }
+    
 }
+
+
